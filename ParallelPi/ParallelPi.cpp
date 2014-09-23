c06bbc6fdd25a6a454fa851f2bbac2fc3bea496d
erkkiperkele <aymeric.grail@gmail.com>
2014-09-23 00:03:29 -0400

    Parallel seems working

--- ParallelPi/ParallelPi.cpp
+++ ParallelPi/ParallelPi.cpp
@@ -19,10 +19,24 @@ int main(int argc, char* argv[])
 	MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
 
 	int npoints = 1000000;
-	int circle_count = 0;
+	int circle_count[mpiSize];
 	int i = 0;
+	int count = 0;
+	circle_count[mpiRank] = 0;
 
-	while(i< npoints)
+	if (npoints % mpiSize != 0)
+	{
+		cout << endl << "please enter a multiple value of " << npoints << endl;
+		return 1;
+	}
+
+	int num = npoints / mpiSize;
+
+	//p = number of tasks
+	//find out if I am MASTER or WORKER 
+
+
+	while(i< num)
 	{
 		double x = (double)rand() / (double)RAND_MAX;		
 		double y = (double)rand() / (double)RAND_MAX;		
@@ -30,15 +44,48 @@ int main(int argc, char* argv[])
 
 		if (isInCircle)
 		{
-			++circle_count;
+			++circle_count[mpiRank];
 		}
 		++i;
 	}
 
-	double PI = 4 * (double)circle_count / (double)npoints;
+	if (mpiRank != 0)
+	{
+		//Every slave sends its result to root
+		int source = 1;		
+		while (source < mpiSize)
+		{
+			MPI_Send(&circle_count[mpiRank], mpiSize, MPI_INT, 0, 0, MPI_COMM_WORLD);
+			++source;
+		}
+	}
 
-	cout << endl << "number of points: " << npoints << endl;
-	cout << "value of Pi: " << PI << endl;
+	if (mpiRank == 0)
+	{
+		int source = 1;
+		while(source < mpiSize)
+		{
+			MPI_Recv(&circle_count[source], mpiSize, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			count += circle_count[source];
+			++source;
+		}
+
+		count += circle_count[0];		//Adds master's result to the count of all points
+		double PI = 4 * (double)count / (double)npoints;
+
+		cout << endl << "circle_count[0]" << circle_count[0];
+		cout << endl << "circle_count[1]" << circle_count[1];
+		cout << endl << "circle_count[2]" << circle_count[2];
+		cout << endl << "circle_count[3]" << circle_count[3];
+		
+		cout << endl << "count:" << count << endl;
+
+		cout << endl << "number of points: " << npoints << endl;
+		cout << "value of Pi: " << PI << endl;
+	}
+
+	MPI_Finalize();
+	return 0;
 }
 
 bool FindIsInCircle(double x, double y)
